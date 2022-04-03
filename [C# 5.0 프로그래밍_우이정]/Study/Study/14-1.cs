0ea2ed213bafb93d8e90eea04548b9a2ef41e3ba
ijwoo <dndlwjd28@naver.com>
2022-04-04 01:08:00 +0900

    14장

---
+++ [C# 5.0 프로그래밍_우이정]/Study/Study/14-1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    internal class Class14_1
+    {
+        static void Main(string[] args)
+        {
+            Func<int> func_1 = () => 10;
+            Func<int, int> func_2 = (a) => a * 2;
+
+            // 예상 출력 결과 = 70
+            Console.WriteLine(func_1() + func_2(30));
+        }
+    }
+}
---
+++ [C# 5.0 프로그래밍_우이정]/Study/Study/14-2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    internal class Class14_2
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            foreach (int a in array)
+            {
+                //Action action = new Action
+                //(
+                //    delegate()
+                //    {
+                //        Console.WriteLine(a * a);
+                //    }
+                //);
+
+                Action action = new Action( () => { Console.WriteLine(a * a); } );
+
+                action.Invoke();
+            }
+        }
+    }
+}
