692d9181b01620c2049806d570fc58d40bd79f19
ijwoo <dndlwjd28@naver.com>
2022-04-04 00:20:57 +0900

    13장

--- [C# 5.0 프로그래밍_우이정]/Study/Study/13-2.cs
+++ [C# 5.0 프로그래밍_우이정]/Study/Study/13-2.cs
@@ -10,7 +10,7 @@ namespace Study
 
     class Market
     {
-        // 모든 이벤트는 특수한 형태의 delegatedla
+        // 모든 이벤트는 특수한 형태의 delegate임
         public event MyDelegate CustomerEvent;
 
         public void Buysomething(int CustomerNo)
@@ -22,10 +22,17 @@ namespace Study
 
     class Class13_2
     {
+        public static void EventResult(int a)
+        {
+            Console.WriteLine("축하합니다. {0}번째 고객 이벤트에 당첨되셨습니다.", a);
+        }
+
         static void Main(string[] args)
         {
+
+            // 기대 출력값 : 축하합니다. 30번째 고객 이벤트에 당첨되셨습니다.
             Market market = new Market();
-            market.CustomerEvent += new MyDelegate (/*이벤트 처리기를 구현하세요.*/);
+            market.CustomerEvent += new MyDelegate(EventResult);
 
             for (int customerNo = 0; customerNo < 100; customerNo += 10)
                 market.Buysomething(customerNo);
